60cc1d935c38104f6f8e5aa6fe94bec35a185999
sravanthi465 <sravanthi465@gmail.com>
2025-09-04 14:05:09 -0700

    Added new controller known as Category Controller

---
+++ Shopping_Batch24/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping_Batch24.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
